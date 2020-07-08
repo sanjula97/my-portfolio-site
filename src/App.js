b245aabae109a230a690c6cc3be23cbb1f378763
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-08 15:38:50 +0530

    Added Routing and main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Layout, Header, Navigation,Drawer,Content } from 'react-mdl';
+import Main from './components/main'
+
 import './App.css';
 
 function App() {
   return (
-    <div>Hi sanjula</div>
+    <div style={{height: '300px', position: 'relative'}}>
+    <Layout fixedHeader>
+        <Header title={<span><span style={{ color: '/ddd' }}>Area / </span><strong>The Title</strong></span>}>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+          <Main />
+        </Content>
+    </Layout>
+</div>
   );
 }
 
