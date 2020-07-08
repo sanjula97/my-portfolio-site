fab3ea11e46f05c256c1c0efa91c31256f33cd45
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-08 18:57:17 +0530

    Modified the landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class LandingPage extends Component{
     render(){
         return(
-            <div>LandingPage</div>
+            <div style={{width:'100%', margin:'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                    <img className="awatar-img" src="https://image.flaticon.com/icons/png/512/924/924915.png" alt="My picture"/>
+
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+
+                            <hr />
+
+                            <p>HTML/CSS | JavaScript | ReactJS | NodeJS | Express | JAVA | MongoDB</p>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         );
     }
 }
