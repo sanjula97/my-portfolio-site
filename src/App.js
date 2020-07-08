daeb11254920cda6854d26303b083ff8169a8958
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-08 16:58:03 +0530

    MOdified the header

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import { Layout, Header, Navigation,Drawer,Content } from 'react-mdl';
 import Main from './components/main'
 
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div style={{height: '300px', position: 'relative'}}>
-    <Layout fixedHeader>
-        <Header title={<span><span style={{ color: '/ddd' }}>Area / </span><strong>The Title</strong></span>}>
+    <Layout >
+        <Header className="header-color" title="Sanjula Kraunarathna" scroll>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/aboutme">AbouMe</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/aboutme">AbouMe</Link>
             </Navigation>
         </Drawer>
         <Content>
--- src/components/main.js
+++ src/components/main.js
@@ -2,10 +2,18 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom'
 
 import LandingPage from './landingpage'
+import Resume from './resume'
+import Projects from './projects'
+import Contact from './contact'
+import AboutMe from './aboutme'
 
 const Main = () => (
     <Switch>
         <Route exact path="/"  component={LandingPage}/>
+        <Route path="/resume"  component={Resume}/>
+        <Route path="/projects"  component={Projects}/>
+        <Route path="/contact"  component={Contact}/>
+        <Route path="/aboutme"  component={AboutMe}/>
     </Switch>
 );
 
